b7be225526f0c6cfc4eb42091c14edcdcb22da3c
eli-h <eli.heineman@gmail.com>
2017-05-08 17:56:08 +0000

    Adds conditionals excerises

---
+++ go-outisde.js
@@ -0,0 +1,11 @@
+var tempurature = 28;
+
+if (tempurature < 0) {
+	console.log("Make sure you pick out a scarf!");
+}  else if (tempurature < 15) {
+	console.log("Short sleeves won't cut it!");
+}  else {
+	console.log("Short sleeves are fine.");
+}
+
+console.log("Now you're ready to go outside!");
